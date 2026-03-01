d77cfc582a06e2ca72bf00113c19026b483508a6
h4zm1 <mosbah.hazmi@gmail.com>
2026-03-01 15:09:10 +0100

    feat: setting up base look for sidebar and header

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,8 @@
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>HEADER</h1>
+    </div>
+  );
+};
+export default Header;
--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,49 +1,56 @@
 import { NavLink, Outlet } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 import { LayoutDashboard, FileText, Briefcase, LogOut } from "lucide-react";
+import Header from "./Header";
 
 // main layout wrapper
 const Layout = () => {
   const { user, logout } = useAuth();
 
   return (
-    <div>
-      <NavLink to="/dashboard">ApplyThis</NavLink>
+    <div className="layout">
+      <div className="sidebar">
+        <nav className="sidebar-nav">
+          {/* navlink will auto update styles when 'to' prop match current url*/}
+          <NavLink
+            title="dashboard"
+            to="/dashboard"
+            end // 'end' means exact match only
+          >
+            <LayoutDashboard size={20} />
+            {/* Dashboard */}
+          </NavLink>
 
-      <nav>
-        {/* navlink will auto update styles when 'to' prop match current url*/}
-        <NavLink
-          to="/dashboard"
-          end // 'end' means exact match only
-        >
-          <LayoutDashboard size={20} />
-          Dashboard
-        </NavLink>
+          <NavLink to="/resumes" title="resumes">
+            <FileText size={20} />
+            {/* Resumes */}
+          </NavLink>
 
-        <NavLink to="/resumes">
-          <FileText size={20} />
-          Resumes
-        </NavLink>
+          <NavLink to="/jobs" title="jobs">
+            <Briefcase size={20} />
+            {/* Jobs */}
+          </NavLink>
+        </nav>
 
-        <NavLink to="/jobs">
-          <Briefcase size={20} />
-          Jobs
-        </NavLink>
-      </nav>
-
-      <div>
         <div>
-          <span>{user?.email}</span>
+          {/* <div> */}
+          {/* <span>{user?.email}</span> */}
           <button onClick={logout}>
             <LogOut size={16} />
           </button>
+          {/* </div> */}
         </div>
+        <NavLink className="logo-nav" to="/dashboard">
+          ApplyThis
+        </NavLink>
       </div>
-
       {/* 'outlet' indicate where child routes will render */}
-      <main>
-        <Outlet />
-      </main>
+      <div className="main-holder">
+        <Header />
+        <main className="main-content">
+          <Outlet />
+        </main>
+      </div>
     </div>
   );
 };
