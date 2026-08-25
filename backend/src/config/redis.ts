ec907c423db217248b543cbf3bfb1499804c2208
h4zm1 <mosbah.hazmi@gmail.com>
2026-08-25 22:24:32 +0100

    chore: fix routing issue

--- backend/src/config/redis.ts
+++ backend/src/config/redis.ts
@@ -4,6 +4,15 @@ import logger from "./logger";
 // this's like a giant hashmap, that lives outside the app and persist data
 const redis = new Redis(process.env.REDIS_URL!, {
   maxRetriesPerRequest: null, // required by BullMQ for blockking commands
+  lazyConnect: true, // don't connect on startup
+  // limit rety attempts to avoid log spam
+  retryStrategy(times) {
+    if (times > 3) {
+      logger.warn("redis unavailable");
+      return 30000; // wait 30 sec
+    }
+    return Math.min(times * 1000, 3000); // ioredis increment an internal counter before passing it to retrystrat
+  },
 });
 
 redis.on("connect", () => {
@@ -11,7 +20,7 @@ redis.on("connect", () => {
   logger.info("redis connected");
 });
 
-redis.on("error", (err) => {
+redis.on("err	or", (err) => {
   logger.error({ message: err.message }, "redis error");
 });
 
